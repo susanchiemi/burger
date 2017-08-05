583d3151f680160c60df2646fd5a6e7fb6457260
Susan Chiemi <Susan@Users-MacBook.local>
2017-08-04 23:11:36 -0400

    more commit

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.set("view engine", "handlebars");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 8889,
   host: "localhost",
   user: "root",
   password: "root",
