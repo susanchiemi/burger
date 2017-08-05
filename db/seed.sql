f2f270a7e2ba69ac33e27110c94a6633fbba9340
Susan Chiemi <Susan@Users-MacBook.local>
2017-08-04 22:18:08 -0400

    commit again

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("1", Black Bean Burger, true);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("2", Portobello Burger, true);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("3", Veggie Burger, true);
---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var port = 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 8889,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Serve index.handlebars to the root route.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    res.render("index", { burgers: data });
+  });
+});
+
+
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
